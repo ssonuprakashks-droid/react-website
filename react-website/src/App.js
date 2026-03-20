be7fc9992d892460168e74567a2a6b7bb5eda1d9
ssonuprakashks-droid <ssonuprakashks@gmail.com>
2026-03-20 15:07:48 +0530

    hi

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,11 +1,25 @@
-import react from 'react';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to My React Website</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
